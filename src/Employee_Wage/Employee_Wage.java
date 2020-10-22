5a570ff1f5c4eb163416ae60674d488395ebfb86
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-22 12:07:21 +0530

    Added Switch Case

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -5,28 +5,28 @@ public class Employee_Wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem");
 
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
 		int attendance = (int) (Math.floor(Math.random() * 10) % 3);
 		System.out.println(attendance);
 		int empWagePerHour = 20;
 		int empHrs = 0;
 		int empDailyWage = 0;
-		if (attendance == IS_FULL_TIME) {
+		switch (attendance) {
+		case 0:
+			System.out.println("Employee is absent");
+			System.out.println();
+			break;
+		case 1:
 			System.out.println("Employee is present");
 			System.out.println();
 			empHrs = 8;
-		} else if (attendance == IS_PART_TIME) {
+			break;
+		case 2:
 			System.out.println("Employee is present for Part time");
 			System.out.println();
 			empHrs = 4;
-
-		} else {
-			System.out.println("Employee is absent");
-			System.out.println();
+			break;
 		}
 		empDailyWage = empWagePerHour * empHrs;
 		System.out.println("Employee's salary for the day is " + empDailyWage);
 	}
-
 }
