2e32087c28e2b478d4ef500bd03679e0efd9eeb9
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-22 11:08:22 +0530

    Added Welcome msg

---
+++ src/Employee_Wage/Employee_Wage.java
@@ -0,0 +1,10 @@
+package Employee_Wage;
+import java.util.*;
+public class Employee_Wage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Problem");
+
+
+}
+}
