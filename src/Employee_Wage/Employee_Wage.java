2000a23c15c2684e3d17e23c5033f36f61a11795
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-22 11:15:44 +0530

    Added Employee Present Absent

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -5,6 +5,11 @@ public class Employee_Wage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem");
 
-
-}
+		int IS_FULL_TIME = 1;
+		int attendance = (int) (Math.round(Math.random()));
+		if (attendance == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+   }
 }
