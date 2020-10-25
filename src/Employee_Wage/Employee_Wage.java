39807067b660bf8dc26965359aed1d4109616e36
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-25 14:35:10 +0530

    Added Employee Wage For Multiple Companies

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -1,16 +1,23 @@
 package Employee_Wage;
 public class Employee_Wage {
-	static int workingDaysInMonth = 20;
-	static int workingHoursinMonth = 100;
-	static int empWagePerHour = 20;
+	
+
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Problem");
-		int totalMonthlyWage = empWageForMonth();
-		System.out.println("Employee's salary for the month is " + totalMonthlyWage);
+		System.out.println("Welcome to Employee Problem");
+		int demart = empWage("demart" ,20,2,10);
+		int Reliance = empWage("Reliance",21,4,20);
+		
+		
+		System.out.println("for Dmart " + demart);
+		System.out.println("For Reliance " + Reliance);
+
+	
 	}
 
-	public static int empWageForMonth() {
+	
+	public static int empWage(String name, int empWagePerHour, int workingDaysInMonth, int workingHoursinMonth) {
+	
 		int end = 0;
 		int empHrs = 0;
 		int empDailyWage = 0;
@@ -28,13 +35,11 @@ public class Employee_Wage {
 				empHrs = 0;
 				break;
 			case 1:
-				System.out.println("Employee is present");
-				System.out.println();
+				System.out.println("Employee is present\n");
 				empHrs = 8;
 				break;
 			case 2:
-				System.out.println("Employee is present for Part time");
-				System.out.println();
+				System.out.println("Employee is present for Part time \n");
 				empHrs = 4;
 				break;
 			}
